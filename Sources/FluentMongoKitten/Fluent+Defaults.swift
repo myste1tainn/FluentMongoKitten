1ad2e01a02161f3f85c21a9d7e3fce39ac1db2c8
Arnon Keereena <a.keereena@gmail.com>
2019-03-12 21:07:49 +0700

    Add ability to insert collection data.

--- Sources/FluentMongoKitten/Fluent+Defaults.swift
+++ Sources/FluentMongoKitten/Fluent+Defaults.swift
@@ -18,34 +18,58 @@ extension Fluent {
   
   // MARK: - Static queries
   
+  @discardableResult
+  public static func insert(models: [Self]) -> [EventLoopFuture<InsertReply>] {
+    return models.map { $0.insert() }
+  }
+  
+  @discardableResult
   public static func find() -> MappedCursor<FindCursor, Self?> {
     return self.collection.find()
                           .map { Self(document: $0) }
   }
   
+  @discardableResult
   public static func update(where query: Query, document: Document) -> EventLoopFuture<UpdateReply> {
     return Self.collection.update(where: query, to: document)
   }
   
+  @discardableResult
   public static func update(models: [Self]) -> [EventLoopFuture<UpdateReply>] {
     return models.map { $0.update() }
   }
   
+  @discardableResult
   public static func delete(models: [Self]) -> [EventLoopFuture<Int>] {
     return models.map { $0.delete() }
   }
   
+  @discardableResult
   public static func delete(where query: Query) -> EventLoopFuture<Int> {
     return Self.collection.deleteAll(where: query)
   }
   
   // MARK: - instance queries
   
+  @discardableResult
+  public func insert() -> EventLoopFuture<InsertReply> {
+    return Self.collection.insert(self.document)
+  }
+  
+  @discardableResult
   public func update() -> EventLoopFuture<UpdateReply> {
     return Self.collection.update(where: "id" == id, to: self.document)
   }
   
+  @discardableResult
   public func delete() throws -> EventLoopFuture<Int> {
     return Self.collection.deleteOne(where: "id" == id)
+  }
+  
+  // MARK: - Alias / Shorthand
+  
+  @discardableResult
+  public func save() -> EventLoopFuture<InsertReply> {
+    return insert()
   }
 }
--- Sources/FluentMongoKitten/Fluent.swift
+++ Sources/FluentMongoKitten/Fluent.swift
@@ -18,20 +18,33 @@ public protocol Fluent: DocumentMappable {
   
   static func setCollection(database: Database)
   
+  @discardableResult
+  static func insert(models: [Self]) -> [EventLoopFuture<InsertReply>]
+  
+  @discardableResult
   static func find(where query: Query) -> MappedCursor<FindCursor, Self>
   
+  @discardableResult
   static func update(where query: Query, document: Document) -> EventLoopFuture<UpdateReply>
   
+  @discardableResult
   static func update(models: [Self]) -> [EventLoopFuture<UpdateReply>]
   
+  @discardableResult
   static func delete(where query: Query) -> EventLoopFuture<Int>
   
+  @discardableResult
   static func delete(models: [Self]) -> [EventLoopFuture<Int>]
   
   init(where query: Query)
   
+  @discardableResult
+  func insert() -> EventLoopFuture<InsertReply>
+  
+  @discardableResult
   func update() -> EventLoopFuture<UpdateReply>
   
+  @discardableResult
   func delete() -> EventLoopFuture<Int>
 }
 
